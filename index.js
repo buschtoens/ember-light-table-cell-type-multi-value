e7c3405f94c2b0c52c8b3cd59f4e4c649f21c135
Jan Buschtöns <buschtoens@gmail.com>
2017-07-20 14:24:11 +0200

    deps: add and configure babel-plugin-transform-class-properties

--- index.js
+++ index.js
@@ -2,5 +2,17 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-light-table-cell-type-multi-value'
+  name: 'ember-light-table-cell-type-multi-value',
+
+  init: function() {
+    this._super.init && this._super.init.apply(this, arguments);
+
+    this.options = this.options || {};
+    this.options.babel = this.options.babel || {};
+    this.options.babel.plugins = this.options.babel.plugins || [];
+
+    if (this.options.babel.plugins.indexOf('transform-class-properties') === -1) {
+      this.options.babel.plugins.push('transform-class-properties');
+    }
+  }
 };
