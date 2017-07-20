45d0400ce017ada92b88bcf65aa07815d7621061
Jan Buschtöns <buschtoens@gmail.com>
2017-07-20 16:56:33 +0200

    build: remove babel config from index
    https://github.com/rwjblue/ember-decorators/issues/134

--- index.js
+++ index.js
@@ -2,21 +2,5 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-light-table-cell-type-multi-value',
-
-  init: function() {
-    this._super.init && this._super.init.apply(this, arguments);
-
-    this.options = this.options || {};
-    this.options.babel = this.options.babel || {};
-    this.options.babel.plugins = this.options.babel.plugins || [];
-
-    if (this.options.babel.plugins.indexOf('transform-decorators-legacy') === -1) {
-      this.options.babel.plugins.push('transform-decorators-legacy');
-    }
-
-    if (this.options.babel.plugins.indexOf('transform-class-properties') === -1) {
-      this.options.babel.plugins.push('transform-class-properties');
-    }
-  }
+  name: 'ember-light-table-cell-type-multi-value'
 };
